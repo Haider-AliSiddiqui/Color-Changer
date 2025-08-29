fec32e549eae4237598e5e9239284e69c9ba79d1
Haider Ali Siddiqui <haideralisiddiqui008@gmail.com>
2025-08-29 15:40:24 +0500

    html js css file created

---
+++ app.js
@@ -0,0 +1,66 @@
+const bodyEL = document.getElementById("body");
+const h1EL = document.getElementById("h1");
+
+const btnRed = document.getElementById("red");
+const btnGreen = document.getElementById("green");
+const btnBlue = document.getElementById("blue");
+const btnOrange = document.getElementById("orange");
+const btnPurple = document.getElementById("purple");
+const btnPink = document.getElementById("pink");
+const btnBrown = document.getElementById("brown");
+const btnYellow = document.getElementById("yellow");
+const btngary = document.getElementById("gary");
+
+
+function ColorRed() {
+  bodyEL.style.backgroundColor = "#FF0000";
+  h1EL.style.backgroundColor = "#FF0000";
+  h1EL.style.color = "#fff";
+}
+function ColorGreen() {
+  bodyEL.style.backgroundColor = "#008000";
+  h1EL.style.backgroundColor = "#008000";
+  h1EL.style.color = "#fff";
+}
+
+function ColorBlue() {
+  bodyEL.style.backgroundColor = "#0000FF";
+  h1EL.style.backgroundColor = "#0000FF";
+  h1EL.style.color = "#fff";
+}
+
+function ColorOrange() {
+  bodyEL.style.backgroundColor = "#FFA500";
+  h1EL.style.backgroundColor = "#FFA500";
+  h1EL.style.color = "#fff";
+}
+
+function ColorPurple() {
+  bodyEL.style.backgroundColor = "#800080";
+  h1EL.style.backgroundColor = "#800080";
+  h1EL.style.color = "#fff";
+}
+
+function ColorPink() {
+  bodyEL.style.backgroundColor = "#FFC0CB";
+  h1EL.style.backgroundColor = "#FFC0CB";
+  h1EL.style.color = "#000";
+}
+
+function ColorBrown() {
+  bodyEL.style.backgroundColor = "#af2a2a";
+  h1EL.style.backgroundColor = "#af2a2a";
+  h1EL.style.color = "#fff";
+}
+
+function ColorYellow() {
+  bodyEL.style.backgroundColor = "#ffff00";
+  h1EL.style.backgroundColor = "#ffff00";
+  h1EL.style.color = "#fff";
+}
+
+function ColorGray() {
+  bodyEL.style.backgroundColor = "gray";
+  h1EL.style.backgroundColor = "gray";
+  h1EL.style.color = "#fff";
+}
